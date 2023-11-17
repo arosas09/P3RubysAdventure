a55999bd006a24cd174c083cd7b3af8874048327
arosas09 <arosas23001@gmail.com>
2023-11-17 11:39:10 -0800

    Finished health collectible script
    I finished the health collectible script and started the damage object script and resized it.

--- RubysAdventureRosas/Assets/Scripts/DamageZone.cs
+++ RubysAdventureRosas/Assets/Scripts/DamageZone.cs
@@ -2,23 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectible : MonoBehaviour
-
+public class DamageZone : MonoBehaviour
 {
-
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
 
         if (controller != null)
         {
-            if (controller.health < controller.maxHealth)
-            {
-                controller.ChangeHealth(1);
-                Destroy(gameObject);
-            }
+            controller.ChangeHealth(-1);
         }
-
     }
-
 }
